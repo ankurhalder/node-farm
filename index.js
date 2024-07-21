6452dad6bbc89a423f2cff450ead5de858efc918
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-21 16:23:25 +0530

    chore: Update index.js to read input from a file and write output to another file

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const fs = require("fs");
 
-const hello = "Hello World";
-console.log(hello);
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+
+console.log(textIn);
+
+const textOut = `this is what we what we know about avocado: ${textIn}.\n Created on ${Date.now()}`;
+
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File Written");
+
+// const hello = "Hello World";
+// console.log(hello);
